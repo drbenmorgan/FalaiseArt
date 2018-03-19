caa3b92f706a75b9103603dd3f436f605522c74b
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2018-03-19 21:14:44 +0000

    Start on basic data model
    NB: checksumming of classes does not seem to work just yet,
    need to check exact proceedure upstream.

---
+++ FLArt/RawDataProducts/RawGeigerHit.h
@@ -0,0 +1,12 @@
+#ifndef FL_DATAMODELS_RAWGEIGERHIT_H
+#define FL_DATAMODELS_RAWGEIGERHIT_H
+
+namespace FL {
+namespace RawDataProducts {
+struct RawGeigerHit {
+};
+} // namespace data
+} // namespace fl
+
+#endif // FL_DATAMODELS_RAWGEIGERHIT_H
+
---
+++ FLArt/RawDataProducts/classes.h
@@ -0,0 +1,8 @@
+// FLArt_RawDataProducts dictionary
+// Our products
+#include "FLArt/RawDataProducts/RawGeigerHit.h"
+#include "FLArt/RawDataProducts/RawGeigerHitCollection.h"
+
+// Needed art/canvas wrappers
+#include "canvas/Persistency/Common/Wrapper.h"
+
