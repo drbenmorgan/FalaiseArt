9eefabaa0ff4f9fa8a6364bf2ae33e0a15afa01b
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2018-04-18 11:23:53 +0100

    Experimental dictionary generation for Bayeux

---
+++ FLFacade/Dictionaries/Bayeux/classes.h
@@ -0,0 +1,8 @@
+#include "canvas/Persistency/Common/Wrapper.h"
+
+#include "bayeux/datatools/i_serializable.h"
+#include "bayeux/datatools/i_tree_dump.h"
+#include "bayeux/datatools/i_clear.h"
+
+#include "bayeux/datatools/event_id.h"
+#include "bayeux/datatools/properties.h"
