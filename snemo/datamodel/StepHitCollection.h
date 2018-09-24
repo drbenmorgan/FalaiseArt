7b7b96e0fb8df68428c925780c7a7a64e6c78a0c
Ben Morgan <ben.morgan@warwick.ac.uk>
2018-09-24 13:05:32 +0100

    Generate Art dictionary for MCTools types
    To ease translation between Bayeux and Art, allow needed Bayeux
    types to be directly stored in Art by generating dictionaries.
    
    Only generate for types known directly from mctools input, and
    only wrap types that need direct storage in the art::Event.
    
    A workaround is provided for mctools::base_step_hit due to Art's
    requirement that directly stored types do not have "_" in the type
    names.

---
+++ snemo/datamodel/StepHitCollection.h
@@ -0,0 +1,12 @@
+#ifndef STEP_HIT_COLLECTION_HH
+#define STEP_HIT_COLLECTION_HH
+
+#include <vector>
+#include "snemo/datamodel/StepHit.h"
+
+namespace snemo {
+using StepHitCollection = std::vector<StepHit>;
+} // namespace snemo
+
+#endif // STEP_HIT_COLLECTION_HH
+
---
+++ snemo/datamodel/classes.h
@@ -0,0 +1,14 @@
+#include "canvas/Persistency/Common/Wrapper.h"
+
+
+// Need to include entire inheritance chain
+#include "bayeux/datatools/i_serializable.h"
+#include "bayeux/datatools/i_tree_dump.h"
+#include "bayeux/datatools/i_clear.h"
+
+#include "bayeux/datatools/event_id.h"
+#include "bayeux/datatools/properties.h"
+#include "bayeux/geomtools/geom_id.h"
+
+#include "snemo/datamodel/StepHit.h"
+#include "snemo/datamodel/StepHitCollection.h"
